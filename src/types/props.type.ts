bf32980749ceb380b079a8458cc0f214ad095237
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2023-02-17 11:40:54 -0800

    update props.type comment

--- src/types/props.type.ts
+++ src/types/props.type.ts
@@ -2,7 +2,7 @@
  * Define the props type for the component/pages
  */
 
-// Define the NavBarProps interface for the NavBar component
+// Define the NavBarProps interface for the NavigationBar component
 export interface NavBarProps {
     title: string
 }
